43062a3203550db9c8b9bd73118a4335dab88d11
Per Johansson <ratache@gmail.com>
2015-01-20 16:41:57 +0100

    GREEN

---
+++ test/Car/CarTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Car;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author UndeadAmilo
+ */
+public class CarTest {
+    private Chassi mockChassi;
+    private Wheel mockLf;
+    private Wheel mockRf; 
+    private Wheel mockLb;
+    private Wheel mockRb;
+    private Transmission mockT;
+    private Engine mockEngine ;
+    private Car SUT;
+    
+    public CarTest() {
+        mockChassi = mock(Chassi.class);
+        mockLf = mock(Wheel.class);
+        mockRf = mock(Wheel.class);
+        mockLb = mock(Wheel.class);
+        mockRb = mock(Wheel.class);
+        mockT = mock(Transmission.class);
+        mockEngine = mock(Engine.class);
+        SUT = new Car();
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
